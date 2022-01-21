0a75612031269cac02d9e602a622a2f78112f140
Shardul Negi <shardul.scott@gmail.com>
2022-01-21 21:26:47 +0530

    added more changes

--- SchoolSPA/src/app/app-routing.module.ts
+++ SchoolSPA/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentDashboardComponent } from './student/student-dashboard/student-dashboard.component';
+import { TeacherDashboardComponent } from './teacher/teacher-dashboard/teacher-dashboard.component';
 
 const routes: Routes = [
+  { path: 'student/dashboard', component: StudentDashboardComponent },
+  { path: 'teacher/dashboard', component: TeacherDashboardComponent }
 ];
 
 @NgModule({
